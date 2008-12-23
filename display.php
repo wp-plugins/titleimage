0d19b7b45041058c3ff95d6bb382205730b9634b
pixelshifter <pixelshifter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-23 22:00:01 +0000

    Bugfix Version 0.5.1
    git-svn-id: https://plugins.svn.wordpress.org/titleimage/tags/0.5.1@82147 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ display.php
@@ -0,0 +1,94 @@
+<?php
+
+	prepareTitleimage();
+
+	function prepareTitleimage () {
+
+		global $wpdb, $titleimage;
+
+		if ($titleimage['defaultstyle']) {
+			// Load the default styles if option is set
+			add_action('wp_head', 'loadTitleimageCSS');
+		}
+
+	}
+
+
+
+	function showTitleimage () {
+		
+		// This function has to be called on any template inside the theme
+ 		// where the titleimage should be displayed ... via showTitleimage() ...
+
+		// TODO: Check i NextGEN Gallery is installed
+
+		global $wpdb, $titleimage;
+
+		if (is_paged() || $titleimage['state'] != 'true') {
+			return; // display only if on startpage and display status is 'true'
+		}
+		
+//		print_r ($titleimage);
+
+		$picturelist = $wpdb->get_results("SELECT * FROM $wpdb->nggpictures WHERE galleryid = '".$titleimage['galleryid']."' ORDER BY sortorder ASC");
+
+		if (!$picturelist) {
+
+			print '<h1>Galeriefehler</h1>';
+			return;
+
+		} else {
+
+			$disp_picture_id = null;
+			$disp_imgwidth = $titleimage['imagewidth'];
+			$disp_imgheight = $titleimage['imageheight'];
+			$disp_description = '';
+
+			if ($titleimage['selectmode'] == 'randompic') {
+				$picturelist_key = rand(0,sizeof($picturelist) - 1);
+			} else {
+				$picturelist_key = '0';
+			}
+
+			$disp_picture_id = $picturelist[$picturelist_key]->pid;
+
+			if ($titleimage['showtitle'] == 'true') {
+
+				if ($titleimage['description'] != '') {
+					$disp_description = $titleimage['description'];
+				} else if ($picturelist[$picturelist_key]->description != '') {
+					$disp_description = $picturelist[$picturelist_key]->description;
+				}
+
+				if ($titleimage['url'] != '') {
+					$disp_description = '<a href="'.$titleimage['url'].'">'.stripslashes($disp_description).'</a>';
+				}
+
+			}
+
+		}
+
+?>
+		<div id="Titleimage">
+			<div class="TI_Inner">
+				<div class="TI_Image">
+					<?=nggSinglePicture($disp_picture_id, $disp_imgwidth, $disp_imgheight);?>
+				</div>
+				<?php if ($disp_description != '') { ?>
+				<div class="TI_Description">
+					<p><?=$disp_description?></p>
+				</div>
+				<?php } ?>
+			</div>
+		</div>
+<?php
+	
+	}
+	
+	
+
+	function loadTitleimageCSS () {
+		echo "\n".'<style type="text/css" media="screen">@import "'.TITLEIMAGE_URLPATH.'css/titleimage.css";</style>'."\n";
+	}
+	
+?>
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Titleimage ===
+Contributors: Marc Schmidt
+Donate link: 
+Tags: image, post
+Requires at least: 2.7
+Tested up to: 2.7
+Stable tag: 0.5.1
+
+This plugin displays a manually or randomly selected image of a chosen gallery from the great plugin NextGEN Gallery.
+
+== Description ==
+
+This plugin is just as simple as it sounds. If you already use the fabulous plugin [NextGEN Gallery](http://wordpress.org/extend/plugins/nextgen-gallery/) than the titleimage plugin gives you the possibility to display the first or a random image from a selected NexGEN Gallery. This selected image will be displayed with the php function call showTitleimage(), which can be used inside your templates. The best place would be inside the index.php file directly after the get_header(); call.
+
+The following parameters are accessible on the Tools->Titleimage administration page:
+
+* State - switches the display of the titleimage on or off
+* Gallery - select the NG Gallery
+* Image selection - show the first or a random image from the selected gallery
+* Max. image width &amp; Max. image height - ...
+* Imagetitle - Switch the display of an image title on or off
+* Title - an alternative image title (overwrites the image title given by the NextGEN Plugin)
+* URL - A link for the title
+* Style - Defines wether the default CSS or a individual CSS File should be used
+
+You will find more information on [pixelshifter.de](http://www.pixelshifter.de), but for now in german language only ... sorry for that.
+
+== Installation ==
+
+1. Upload `titleimage` directory to `/wp-content/plugins/`
+2. Activate the plugin on the 'Plugins' area in your WordPress installation
+3. Use the php function call showTitleimage(); inside of your template
+
+Please keep in mind, that this plugin also requires the NextGEN Gallery plugin: [NextGEN Gallery](http://wordpress.org/extend/plugins/nextgen-gallery/)!
+
+== Frequently Asked Questions ==
+http://www.pixelshifter.de/titleimage
+
